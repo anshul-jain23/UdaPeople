b2cbf400cc1639f4dacf79356378d7a0b9822d84
Anshul <53anshul@gmail.com>
2023-04-28 17:01:37 +0530

    full setup script prometheus

--- setupPrometheus.sh
+++
@@ -1,7 +0,0 @@
-SERVER="34.199.232.199"
-scp -i ./.ssh/Udacity_keyPair.pem ./Prometheus/install.sh ubuntu@$SERVER:/home/ubuntu
-scp -i ./.ssh/Udacity_keyPair.pem ./Prometheus/restart.sh ubuntu@$SERVER:/home/ubuntu
-ssh -i ./.ssh/Udacity_keyPair.pem ubuntu@$SERVER
-scp -i ./.ssh/Udacity_keyPair.pem ./Prometheus/prometheus.yml ubuntu@$SERVER:/etc/prometheus/
-scp -i ./.ssh/Udacity_keyPair.pem ./Prometheus/prometheus.service ubuntu@$SERVER:/etc/systemd/system/
-ssh -i ./.ssh/Udacity_keyPair.pem ubuntu@$SERVER
